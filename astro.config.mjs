0c149230121c919ae5af162097a02261f5cd3b02
Martin <martinplr@gmail.com>
2023-08-21 23:01:15 +0200

    feat: init config and packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,29 @@
-import { defineConfig } from 'astro/config';
-
-import alpinejs from "@astrojs/alpinejs";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+function alpine() {
+    return {
+        name: "alpinejs",
+        hooks: {
+            "astro:config:setup": ({ injectScript }) => {
+                injectScript(
+                    "page",
+                    `
+                    import Alpine from 'alpinejs'; 
+                    import persist from '@alpinejs/persist'
+                    Alpine.plugin(persist)
+                    Alpine.$dispatch = function (name, detail = '') {
+                        window.dispatchEvent(new CustomEvent(name, { detail: detail, bubbles: true }))
+                    }
+                    window.Alpine = Alpine; 
+                    Alpine.start();
+                `
+                )
+            },
+        },
+    }
+}
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs()]
-});
+    integrations: [alpine(), tailwind()],
+})
